560ce3eb607ee4ab10a2c370c28f9014b39cbc09
Oskar Straczkowski <o.straczkowski@gmail.com>
2016-11-12 00:17:56 +0100

    Added implementation of StatisticsCalculator.

--- src/main/java/pl/parser/nbp/StatisticsCalculator.java
+++ src/main/java/pl/parser/nbp/StatisticsCalculator.java
@@ -10,21 +10,29 @@ import java.util.Arrays;
  */
 class StatisticsCalculator {
 
-    static double average(final double... values) {
+    /**
+     * @param values values to calculate the statistic
+     * @return average of given values or NaN
+     */
+    static Double average(final double... values) {
         if (values.length == 0) {
             throw new IllegalArgumentException("Unexpected empty list");
         }
         return Arrays.stream(values).summaryStatistics().getAverage();
     }
 
-    static double stdDeviation(final double... values) {
+    /**
+     * @param values values to calculate the statistic
+     * @return standard deviation value or NaN
+     */
+    static Double stdDeviation(final double... values) {
         if (values.length == 0) {
             throw new IllegalArgumentException("Unexpected empty list");
         }
 
         final double average = average(values);
         if (average == Double.NaN) {
-            throw new IllegalArgumentException("Average value NaN");
+            throw new IllegalArgumentException("Average value must be an actual value but is NaN");
         }
 
         double sumOfDeviations = Arrays.stream(values).reduce(0, (sum, x) -> sum + (x - average) * (x - average));
--- src/test/java/pl/parser/nbp/StatisticsCalculatorTests.java
+++ src/test/java/pl/parser/nbp/StatisticsCalculatorTests.java
@@ -4,13 +4,45 @@
 package pl.parser.nbp;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class StatisticsCalculatorTests {
 
-    private StatisticsCalculator calculator;
+    private double[] testValues;
+    private Double expectedAverage;
+    private Double expectedStdDeviation;
+
+    public StatisticsCalculatorTests(final double[] testValues, final Double average, final Double stdDeviation) {
+        this.testValues = testValues;
+        this.expectedAverage = average;
+        this.expectedStdDeviation = stdDeviation;
+    }
+
+    @Parameterized.Parameters
+    public static Collection getTestParameters() {
+        return Arrays.asList(new Object[][] {
+                { new double[] { 0.1 }, 0.1, 0.0 },
+                { new double[] { 0.1, 0.9 }, 0.5, 0.4 },
+                { new double[] { 600.0, 470.0, 170.0, 430.0, 300.0 }, 394.0, 147.32277488562318 }
+        });
+    }
 
     @Test
-    public void testCalculate() {
-        // TODO: implement
+    public void testAverageCalculation() {
+        final Double actualResult = StatisticsCalculator.average(testValues);
+        assertEquals(expectedAverage, actualResult);
+    }
+
+    @Test
+    public void testStdDeviationCalculation() {
+        final Double actualResult = StatisticsCalculator.stdDeviation(testValues);
+        assertEquals(expectedStdDeviation, actualResult);
     }
 }
