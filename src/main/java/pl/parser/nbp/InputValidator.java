9c5302656a0d5b28b57b7f96495f95318dfad84c
Oskar Straczkowski <o.straczkowski@gmail.com>
2016-11-14 01:51:54 +0100

    Added null checks to InputValidator.

--- src/main/java/pl/parser/nbp/InputValidator.java
+++ src/main/java/pl/parser/nbp/InputValidator.java
@@ -3,11 +3,14 @@
  */
 package pl.parser.nbp;
 
+import javax.annotation.Nonnull;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Utility class with methods to validate input arguments.
@@ -19,6 +22,7 @@ class InputValidator {
     private final static String EARLIEST_DATE_AVAILABLE_STRING = "2002-01-02";
     private final static DateTimeFormatter DTF = DateTimeFormatter.ofPattern(DATE_FORMAT);
     private final static LocalDate EARLIEST_DATE_AVAILABLE;
+
     static {
         try {
             EARLIEST_DATE_AVAILABLE = LocalDate.parse(EARLIEST_DATE_AVAILABLE_STRING, DTF);
@@ -33,7 +37,9 @@ class InputValidator {
      * @param args arguments from command line which describes expected query.
      * @return POJO object with parameters of the request which has to be made.
      */
-    boolean isInputValid(final String[] args) {
+    boolean isInputValid(@Nonnull final String[] args) {
+        requireNonNull(args, "'args' must not be null");
+
         if (args.length != 3) {
             errorMessage = "Invalid number of arguments, expected <currency> <date from> <date to> (e.g. EUR 2013-01-28 2013-01-31)";
             return false;
@@ -45,7 +51,9 @@ class InputValidator {
         return true;
     }
 
-    private boolean isCurrencyValid(final String currency) {
+    private boolean isCurrencyValid(@Nonnull final String currency) {
+        requireNonNull(currency, "'currency' must not be null");
+
         final Pattern pattern = Pattern.compile("[a-zA-Z]{3}");
         final Matcher matcher = pattern.matcher(currency);
         if (!matcher.matches()) {
@@ -55,16 +63,22 @@ class InputValidator {
         return true;
     }
 
-    // TODO: implement endDate <= Year.now()
-    private boolean areDatesValid(final String dateFromString, final String dateToString) {
-        final LocalDate dateFrom = LocalDate.parse(dateFromString, DTF);
-        final LocalDate dateTo = LocalDate.parse(dateToString, DTF);
-        if (dateFrom.isBefore(EARLIEST_DATE_AVAILABLE)) {
+    private boolean areDatesValid(@Nonnull final String startDateString, @Nonnull final String endDateString) {
+        requireNonNull(startDateString, "'startDateString' must not be null");
+        requireNonNull(endDateString, "'endDateString' must not be null");
+
+        final LocalDate startDate = LocalDate.parse(startDateString, DTF);
+        final LocalDate endDate = LocalDate.parse(endDateString, DTF);
+        if (startDate.isAfter(endDate)) {
+            errorMessage = "Start date is after end date";
+            return false;
+        }
+        if (startDate.isBefore(EARLIEST_DATE_AVAILABLE)) {
             errorMessage = "Start date is later than earliest date available (" + EARLIEST_DATE_AVAILABLE_STRING + ")";
             return false;
         }
-        if (dateFrom.isAfter(dateTo)) {
-            errorMessage = "Start date is after end date";
+        if (endDate.isAfter(LocalDate.now())) {
+            errorMessage = "End date is from the future";
             return false;
         }
         return true;
